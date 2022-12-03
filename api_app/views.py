990d9d3543195de6c414dda24f7f3dca41e5e06d
MelvinPariyadan <21imc10063@fh-krems.ac.at>
2022-12-03 18:18:40 +0100

    28-create-login-endpoint

--- api_app/views.py
+++ api_app/views.py
@@ -2,6 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+class LoginUser(APIView):
+
+    def post(self,request,format = None):
+        #serializer = self.serializer_class(data=request.data)
+        try:
+            user = request.data['username']
+            password = request.data['password']
+            print(user,password)
+            
+            m = User.objects.get(username=user)
+            matchcheck = check_password(password,m.password)
+            if matchcheck:
+                self.request.session['member_id'] = m.id
+                return HttpResponse("You are logged in")
+                return HttpResponseRedirect('/you-are-logged-in/')
+            else:
+                raise User.DoesNotExist
+        except User.DoesNotExist:
+            return HttpResponse("Your username and password didn't match.")
+        except:
+            return Response({'Bad Request': 'User already exists'}, status=status.HTTP_403_FORBIDDEN)
+
+        
+
+
 class LogoutUser(APIView):
 
     def post(self,request,format = None):
