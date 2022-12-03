865117e2b99b7031718116d1cca0f033ed04a23b
MelvinPariyadan <21imc10063@fh-krems.ac.at>
2022-12-03 18:12:31 +0100

    24-create-logout-endpoint

---
+++ api_app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','first_name','email','password']
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','password']
--- api_app/views.py
+++ api_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class LogoutUser(APIView):
+
+    def post(self,request,format = None):
+        try:
+            del self.request.session['member_id']
+        except KeyError:
+            pass
+        
+        return HttpResponse("You're logged out.")
