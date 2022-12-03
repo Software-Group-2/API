16d855fa892a7b2a9ea8a29a968705d1eedaae55
MelvinPariyadan <21imc10063@fh-krems.ac.at>
2022-12-03 18:23:38 +0100

    23-create-registration-endpoint

--- api_app/views.py
+++ api_app/views.py
@@ -1,7 +1,50 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from django.contrib.auth.models import User
+
+
+from django.contrib.auth.hashers import check_password
+from .serializers import CreateUserSerializer,LoginUserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+
 
 # Create your views here.
 
+class CreateUser(APIView):
+    serializer_class = CreateUserSerializer
+
+    def post(self,request,format = None):
+
+        serializer = self.serializer_class(data=request.data)
+   
+        if serializer.is_valid():
+            print(serializer.data)
+            username = serializer.data.get('username')
+            first_name = serializer.data.get('first_name')
+            print(first_name)
+            email = serializer.data.get('email')
+            password = serializer.data.get('password')
+            queryset = User.objects.filter(username = username)
+            queryset2 = User.objects.filter(email = email)
+
+            if queryset.exists() or queryset2.exists():
+                return  Response({'Bad Request': 'Username or email already exists'}, status=status.HTTP_403_FORBIDDEN)
+            else:
+                user = User(username = username,first_name = first_name,email = email,password = password)
+                user.save()
+                self.request.session['member_id'] = user.id
+                return Response(CreateUserSerializer(user).data, status=status.HTTP_201_CREATED)
+
+
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+            
 
 class LoginUser(APIView):
 
