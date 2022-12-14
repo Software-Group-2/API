9b37ff83fc627c71a56a36b0a860d6923a85c581
Kaloyan-Stoyanov <79645779+Kaloyan-Stoyanov@users.noreply.github.com>
2022-12-14 18:37:28 +0100

    Update settings.py

--- API/settings.py
+++ API/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "https://www.test-cors.org",
     "http://localhost:19006"
-    "https://ws22-23-swepm3ilv-inf-ba-vz-group-2.github.io/App"
+    "https://ws22-23-swepm3ilv-inf-ba-vz-group-2.github.io"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
