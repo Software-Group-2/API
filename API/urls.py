caaba010aadadf1e84bcc53c0e57fafd682e1754
MelvinPariyadan <21imc10063@fh-krems.ac.at>
2022-12-16 09:49:36 +0100

    68-better-way-to-doc

--- API/urls.py
+++ API/urls.py
@@ -15,6 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Unreveal API",
+      default_version='v1',
+      description="API Documentation of Unreveal App",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+   authentication_classes=[]
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
